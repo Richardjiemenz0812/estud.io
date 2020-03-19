884629b1cfeb8166fcf018c0e735359b4d2b2b6a
Richard <richardjimenez@gmail.com>
2020-03-18 23:54:38 -0400

    first commint

---
+++ server.py
@@ -0,0 +1,39 @@
+from flask import *
+from flask_redis import FlaskRedis
+
+app = Flask(__name__)
+r=FlaskRedis(app)
+
+@app.route('/')
+def index():
+    rd=r.scan()
+    list=str(rd).replace("(0, [b","").replace("])","").replace(", b","").replace("","").replace("(0, [","").split("'")
+    print(list)
+    return render_template("index.html",list=list)
+
+@app.route('/search')
+def search():
+    req=request.args.get('q')
+    pos=r.lrange(req,0,-1)
+    list=str(pos).replace("(0, [b","").replace("])","").replace(", b",""
+    ).replace("","").replace("(0, [","").replace("[b","").replace("]","").split("'")
+    print(list)
+    title=list[3]
+    post=list[1]
+    post=str(post).replace("[b'","").replace("']","").replace("\\r\\n","<br>")
+    print(title)
+    return render_template("/search.html",title=title,post=post)
+
+@app.route('/add')
+def add():
+    return render_template("add.html")
+
+@app.route('/addf', methods=['POST'])
+def addf():
+    title=request.form.get('title')
+    post=request.form.get('post')
+    r.lpush(title,title,post)
+    return redirect('/')
+
+
+app.run(debug=True,host="0.0.0.0")
