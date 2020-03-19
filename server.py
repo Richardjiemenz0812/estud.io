df18c0fae73e95f13fd2e440945a893868a6fb1a
Richard <richardjimenez@gmail.com>
2020-03-19 01:20:14 -0400

    2commint

--- server.py
+++ server.py
@@ -1,8 +1,12 @@
 from flask import *
 from flask_redis import FlaskRedis
+import random
+import os
 
 app = Flask(__name__)
 r=FlaskRedis(app)
+
+path_folder='static/imgs'
 
 @app.route('/')
 def index():
@@ -18,11 +22,12 @@ def search():
     list=str(pos).replace("(0, [b","").replace("])","").replace(", b",""
     ).replace("","").replace("(0, [","").replace("[b","").replace("]","").split("'")
     print(list)
-    title=list[3]
-    post=list[1]
+    title=list[5]
+    post=list[3]
+    img=list[1]
     post=str(post).replace("[b'","").replace("']","").replace("\\r\\n","<br>")
     print(title)
-    return render_template("/search.html",title=title,post=post)
+    return render_template("/search.html",title=title,post=post,img=img)
 
 @app.route('/add')
 def add():
@@ -33,6 +38,13 @@ def addf():
     title=request.form.get('title')
     post=request.form.get('post')
     r.lpush(title,title,post)
+    try:
+        fname=str(random.random()) + ".jpg"
+        f = request.files['file']
+        f.save(os.path.join("static/imgs",fname))
+        r.lpush(title,fname)
+    except expression as identifier:
+        pass
     return redirect('/')
 
 
