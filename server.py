817f510bb4500f3dc93a20f3bac165be212ac2f5
Richard <richardjimenez@gmail.com>
2020-03-20 06:18:39 -0400

    add likes and points system

--- server.py
+++ server.py
@@ -30,7 +30,25 @@ def index():
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    user=request.cookies.get("user")
+    if user != None:
+        return redirect("/profile")
+    else:
+        return render_template("/login.html")
+
+@app.route("/profile")
+def profile():
+    user=request.cookies.get("user")
+    if user != None:
+        name=r.hget(user,"name")
+        name=str(name).replace("b'","").replace("'","")
+        pwd=r.hget(user,"pwd")
+        pwd=str(pwd).replace("b'","").replace("'","")
+        pts=r.hget(user,"pts")
+        pts=str(pts).replace("b'","").replace("'","")
+        return render_template("/profile.html",name=name,pwd=pwd,pts=pts)
+    else:
+        return redirect("/login")
 
 @app.route('/login',methods=['POST'])
 def loginp():
@@ -76,35 +94,58 @@ def search():
     q=request.args.get('q')
     con=r.lrange(q,1,1)
     con=str(con).replace("[b'","").replace("']","").replace("\\r\\n","<br>").replace('[b"',"").replace('"]',"")
+    by=r.lrange(q,3,3)
+    by=str(by).replace("[b'","").replace("']","").split(":")
+    print(by)
     try:
         img = r.lrange(q,0,0)
         img=str(img).replace("[b'","").replace("']","")
     except expression as identifier:
         pass
     print()
-    return render_template('search.html',title=q,post=con,img=img)
+    return render_template('search.html',title=q,post=con,img=img,by=by[1])
     
 @app.route('/add')
 def add():
-    return render_template("add.html")
+    user=request.cookies.get("user")
+    if user != None:
+        return render_template("add.html")
+    else:
+        return render_template('/login.html',msg="before write a post you must login")
 
 @app.route('/addf', methods=['POST'])
 def addf():
-    user=request.cookies.get("user")
+    user=str(request.cookies.get("user"))
     print(user)
-    if user != None:
-        title=request.form.get('title')
-        post=request.form.get('post')
-        r.lpush(title,title,post)
-        try:
-            fname=str(random.random()) + ".jpg"
-            f = request.files['file']
-            f.save(os.path.join("static/imgs",fname))
-            r.lpush(title,fname)
-        except expression as identifier:
-            pass
-        return redirect('/')
-    return redirect('/login')
+    title=request.form.get('title')
+    post=request.form.get('post')
+    r.lpush(title,user,title,post)
+    try:
+        fname=str(random.random()) + ".jpg"
+        f = request.files['file']
+        f.save(os.path.join("static/imgs",fname))
+        r.lpush(title,fname)
+    except expression as identifier:
+        pass
+    return redirect('/')
 
+@app.route("/like")
+def like():
+    user=request.cookies.get("user")
+    q=request.args.get("q")
+    p=request.args.get("by")
+    userp="user:"+ p
+    print(userp)
+    pts=0
+    if user != None:
+        if q == "1":
+            pts=r.hget(userp,"pts")
+            print(pts)
+            pts=int(pts)+1
+            print(int(pts))
+            r.hset(userp,"pts",pts)
+            return redirect('/')
+    else:
+        return redirect("/add")
 
 app.run(debug=True,host="0.0.0.0")
