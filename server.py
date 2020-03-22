089c3db8a3823343ced335e5977e1f42e5f7cee1
Richard <richardjimenez@gmail.com>
2020-03-22 15:32:49 -0400

    deleted post

--- server.py
+++ server.py
@@ -38,58 +38,61 @@ def login():
     else:
         return render_template("/login.html")
 
+
 @app.route("/profile")
 def profile():
-    user=session["user"]
-    if user != None:
-        name=r.hget(user,"name")
-        name=str(name).replace("b'","").replace("'","")
-        pwd=r.hget(user,"pwd")
-        pwd=str(pwd).replace("b'","").replace("'","")
-        pts=r.hget(user,"pts")
-        pts=str(pts).replace("b'","").replace("'","")
-        rd=r.scan()
-        list=str(rd).replace("(0, [b","").replace("])","").replace(", b","").replace("","").replace("(0, [","").split("'")
-        print(list)
-        pl=""
-        for i in list:
-            print(i)
-            print(r.type(i))
-            print("---------------------")
-            if i != "":
-                type=r.type(i)
-                type=str(type)
-                if type == "b'list'":
-                    print("es una lista!")
-                    print("########################################")
-                    pl=str(pl) + i + ","
-                    print(pl)
-                    print("##################################")
-                    ub=pl.split(",")
-                    print(ub)
-                    list2=""
-                    for u in ub:
-                        if u != "":
-                            print(u)
-                            ulist=r.lrange(u,3,3)
-                            print(ulist)
-                            print("-------------------------------")
-                            print(user)
-                            print("-------------------------------")
-                            ulist=str(ulist)
-                            print("#"+ulist)
-                            ulist=ulist.replace("[b'","").replace("']","")
-                            print("="+ulist)
-                            if user == ulist:
-                                print("exito!!!!!!!!!!!!!!!!!!!!!!!!")
-                                list2=list2+u+","
-                            print(list2)
-                            
-                    
-        y=list2.split(",")
-        n=len(y)
-        n=n-1           
-        return render_template("/profile.html",name=name,pwd=pwd,pts=pts,posts=y,n=n)
+    if "user" in session:
+        user=session["user"]
+        if user != None:
+            name=r.hget(user,"name")
+            name=str(name).replace("b'","").replace("'","")
+            pwd=r.hget(user,"pwd")
+            pwd=str(pwd).replace("b'","").replace("'","")
+            pts=r.hget(user,"pts")
+            pts=str(pts).replace("b'","").replace("'","")
+            rd=r.scan()
+            list=str(rd).replace("(0, [b","").replace("])","").replace(", b","").replace("","").replace("(0, [","").split("'")
+            print(list)
+            pl=""
+            list2=""
+            for i in list:
+                print(i)
+                print(r.type(i))
+                print("---------------------")
+                if i != "":
+                    type=r.type(i)
+                    type=str(type)
+                    if type == "b'list'":
+                        print("es una lista!")
+                        print("########################################")
+                        pl=str(pl) + i + ","
+                        print(pl)
+                        print("##################################")
+                        ub=pl.split(",")
+                        print(ub)
+                        list2=""
+                        for u in ub:
+                            if u != "":
+                                print(u)
+                                ulist=r.lrange(u,3,3)
+                                print(ulist)
+                                print("-------------------------------")
+                                print(user)
+                                print("-------------------------------")
+                                ulist=str(ulist)
+                                print("#"+ulist)
+                                ulist=ulist.replace("[b'","").replace("']","")
+                                print("="+ulist)
+                                if user == ulist:
+                                    print("exito!!!!!!!!!!!!!!!!!!!!!!!!")
+                                    list2=list2+u+","
+                                print(list2)
+                                
+                        
+            y=list2.split(",")
+            n=len(y)
+            n=n-1           
+            return render_template("/profile.html",name=name,pwd=pwd,pts=pts,posts=y,n=n)
     else:
         return redirect("/login")
 
@@ -142,7 +145,7 @@ def search():
     try:
         img = r.lrange(q,0,0)
         img=str(img).replace("[b'","").replace("']","")
-    except expression as identifier:
+    except:
         pass
     print()
     return render_template('search.html',title=q,post=con,img=img,by=by[1])
@@ -168,7 +171,7 @@ def addf():
         f = request.files['file']
         f.save(os.path.join("static/imgs",fname))
         r.lpush(title,fname)
-    except expression as identifier:
+    except:
         pass
     return redirect('/')
 
@@ -258,5 +261,79 @@ def user():
     n=len(y)
     n=n-1          
     return render_template("/view_profile.html",name=user_name,pts=user_pts,posts=y,n=n)
+
+@app.route("/del_post")
+def del_post():
+    if "user" in session:
+        user=session["user"]
+        if user != None:
+            name=r.hget(user,"name")
+            name=str(name).replace("b'","").replace("'","")
+            pwd=r.hget(user,"pwd")
+            pwd=str(pwd).replace("b'","").replace("'","")
+            pts=r.hget(user,"pts")
+            pts=str(pts).replace("b'","").replace("'","")
+            rd=r.scan()
+            list=str(rd).replace("(0, [b","").replace("])","").replace(", b","").replace("","").replace("(0, [","").split("'")
+            print(list)
+            pl=""
+            list2=""
+            ulist=""
+            for i in list:
+                print(i)
+                print(r.type(i))
+                print("---------------------")
+                if i != "":
+                    type=r.type(i)
+                    type=str(type)
+                    if type == "b'list'":
+                        print("es una lista!")
+                        print("########################################")
+                        pl=str(pl) + i + ","
+                        print(pl)
+                        print("##################################")
+                        ub=pl.split(",")
+                        print(ub)
+                        list2=""
+                        for u in ub:
+                            if u != "":
+                                print(u)
+                                ulist=r.lrange(u,3,3)
+                                print(ulist)
+                                print("-------------------------------")
+                                print(user)
+                                print("-------------------------------")
+                                ulist=str(ulist)
+                                print("#"+ulist)
+                                ulist=ulist.replace("[b'","").replace("']","")
+                                print("="+ulist)
+                                if user == ulist:
+                                    print("exito!!!!!!!!!!!!!!!!!!!!!!!!")
+                                    list2=list2+u+","
+                                print(list2)
+                                
+                        
+            y=list2.split(",")
+            n=len(y)
+            n=n-1
+            user=ulist           
+            return render_template("/profile_del.html",name=name,pwd=pwd,pts=pts,posts=y,n=n,user=ulist)
+    else:
+        return redirect("/login")
+
+@app.route("/del")
+def dell():
+    if "user" in session:
+        user=session["user"]
+        q=request.args.get("q")
+        u=request.args.get("user")
+        if u == user:
+            r.delete(q)
+            return redirect("/profile")
+        else:
+            return "este post no es tuyo"
+    else:
+        return redirect("/login")
+
 
 app.run(debug=True,host="0.0.0.0")
