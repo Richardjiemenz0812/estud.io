97f88a75cc0e1ad7afbadb4581593499d93a2ebb
Richard Jimenez <richardjimenez176@gmail.com>
2020-03-28 03:49:43 -0400

    menu

--- static/js/main.js
+++ static/js/main.js
@@ -7,3 +7,22 @@ const butInstall = document.getElementById('butInstall');
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/static/js/sw.js');
 }
+i=0
+function menu(){
+  console.log("yolo")
+  if (i == 0) {
+    document.getElementById("pp1").style.display = "inline"
+    document.getElementById("pp2").style.display = "inline"
+    document.getElementById("pp3").style.display = "inline"
+    document.getElementById("ul-nav").style.width = "30%"
+    document.getElementById("list").style.display = "block"
+    i=1
+  } else {
+    document.getElementById("pp1").style.display = "none"
+    document.getElementById("pp2").style.display = "none"
+    document.getElementById("pp3").style.display = "none"
+    document.getElementById("list").style.display = "none"
+    document.getElementById("ul-nav").style.width = "unset"
+    i=0
+  }
+}
